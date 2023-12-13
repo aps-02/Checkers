e0d9d979adfb2563136d421b224dd248648d0639
aps-02 <114816781+aps-02@users.noreply.github.com>
2023-12-14 01:51:30 +0530

    Add files via upload

---
+++ checkers/constants.py
@@ -0,0 +1,11 @@
+import pygame
+HEIGHT,WIDTH=800,800
+ROWS,COLS=8,8
+SQUARE_SIZE=WIDTH//COLS
+
+RED=(255,0,0)
+WHITE=(255,255,255)
+BLACK=(0,0,0)
+BLUE=(0,0,255)
+OUTCOLOR=(171,118,39)
+CROWN=pygame.transform.scale(pygame.image.load('assets/crown.png'),(44,25))
